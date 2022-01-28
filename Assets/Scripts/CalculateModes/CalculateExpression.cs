06ef52f23a1a6846891365e0290f3974988869c3
TwilightSock <anderman033@gmail.com>
2022-01-28 22:26:38 +0300

    stage 11: change CalculateExpression

--- Assets/Scripts/CalculateModes/CalculateExpression.cs
+++ Assets/Scripts/CalculateModes/CalculateExpression.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
@@ -12,10 +13,44 @@ public class CalculateExpression : MonoBehaviour,ICalculator
     {
         try
         {
-            DataTable datatable = new DataTable();
-            outputController.ShowResult(datatable.Compute(outputController.ReadText(), "").ToString());
+            string expression = outputController.ReadText();
+            string pattern = @"^[-+]?(\d+\.?\d*)+([-|+|*|/]{1})+(\d+\.?\d*)";
+            System.Text.RegularExpressions.MatchCollection matchCollection = System.Text.RegularExpressions.Regex.Matches(expression, pattern);
+            int check = matchCollection.Count;
+            if (check == 0)
+            {
+                throw new Exception("Syntax Error");
+            }
+            foreach (System.Text.RegularExpressions.Match m in
+                System.Text.RegularExpressions.Regex.Matches(expression, pattern))
+            {
+                decimal value1 = Decimal.Parse(m.Groups[1].Value);
+                decimal value2 = Decimal.Parse(m.Groups[3].Value);
+                decimal valueResult;
+                switch (m.Groups[2].Value)
+                {
+                    case "+":
+                        valueResult = value1 + value2;
+                        Debug.Log(valueResult);
+                        outputController.ShowResult(valueResult.ToString());
+                        break;
+                    case "-":
+                        valueResult= value1 - value2;
+                        outputController.ShowResult(valueResult.ToString());
+                        break;
+                    case "*":
+                        valueResult = value1 * value2;
+                        outputController.ShowResult(valueResult.ToString());
+                        break;
+                    case "/":
+                        valueResult = value1 / value2;
+                        outputController.ShowResult(valueResult.ToString());
+                        break;
+                }
+            }
+   
         }
-        catch (SyntaxErrorException ex)
+        catch (Exception ex)
         {
             Debug.Log(ex);
             outputController.ShowResult("Syntax error");
